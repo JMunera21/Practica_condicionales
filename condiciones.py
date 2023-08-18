efd7aea1f91a137330e69dce5860b9bd2b108bea
Alejo <alejandrovanegas09@outlook.com>
2023-08-18 10:15:23 -0500

    NT3 se inicia condicionales

---
+++ condiciones.py
@@ -0,0 +1,16 @@
+# edadusuario=int(input("Digita tu edad: "))
+
+# if edadusuario>=18:
+#     print("Bienvenido a la disco!!")
+    
+# else:
+#     print("Eres menor de edad,pa la casa")  
+#     print("Oeee pa la casa...")
+
+Temperatura=int(input("Digite la temperatura:° "))
+GradosAlcohol=int(input("Digite los grados de alcohol: "))
+
+if Temperatura>50 or GradosAlcohol>5:
+    print("Alarma")
+else:
+    print ("Normal")
